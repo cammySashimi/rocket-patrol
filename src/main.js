449fedaa21cf60cba3fdbb1fdfa03b9df44dac2a
Camellia Shea <cashmill@ucsc.edu>
2020-04-19 13:20:35 -0700

    Revert "Mod started: Super Dinosaur Rehydrator 3000"
    This reverts commit b3fddfad7d5f577ab31bcbed80e0a7caddbdc4a8.

--- src/main.js
+++ src/main.js
@@ -1,17 +1,3 @@
-/*
- * ROCKET PATROL MOD:
- * SUPER DINOSAUR REHYDRATOR 3000
- * 
- * Changes:
- * + Rethemed to be a game about shooting water at dinosaur sponges to rehydrate
- *   them (50 pts)
- * + Simultaneous play for 1-4 players (50 pts)
- * + Bullets added instead of rocket moving
- * + Sprite rendering method replaced with texture atlas
- * 
- * */
-
-
 let config = {
 	type: Phaser.CANVAS,
 	width: 640,
@@ -23,19 +9,8 @@ let game = new Phaser.Game(config);
 
 // define game settings
 game.settings = {
-    numPlayers: 1,
-    gameMode: "Free-for-all",
+    spaceshipSpeed: 3,
     gameTimer: 60000    
 }
 
-// menu control variables
-let key1P, key2P, key3P, key4P;
-let keyCoop, keyComp;
-let keyStart;
-
-// control variables for 4 players
-// 4-player game on one keyboard? that's right baby
-let key1F, key1L, key1R;
-let key2F, key2L, key2R;
-let key3F, key3L, key3R;
-let key4F, key4L, key4R;
+let keyF, keyLEFT, keyRIGHT;
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -5,18 +5,17 @@ class Menu extends Phaser.Scene {
 	}
 	
 	preload() {
-		//this.load.audio("sfx_select", "assets/blip_select12.wav");
-		//this.load.audio("sfx_explosion", "assets/explosion38.wav");
-		//this.load.audio("sfx_rocket", "assets/rocket_shot.wav");
+		this.load.audio("sfx_select", "assets/blip_select12.wav");
+		this.load.audio("sfx_explosion", "assets/explosion38.wav");
+		this.load.audio("sfx_rocket", "assets/rocket_shot.wav");
 	}
 	
 	create() {
-		// styles
-		let menuHeader = {
+		let menuConfig = {
 			fontFamily: "Courier",
-			fontSize: "24px",
-			fontStyle: "bold",
-			color: "#30d80a",
+			fontSize: "28px",
+			backgroundColor: "#F3B141",
+			color: "#843605",
 			align: "right",
 			padding: {
 				top: 5,
@@ -24,111 +23,42 @@ class Menu extends Phaser.Scene {
 			},
 			fixedWidth: 0
 		}
-		
-		let menuBody = {
-			fontFamily: "Courier",
-			fontSize: "18px",
-			color: "#eee",
-			fontStyle: "bold",
-			align: "right",
-			padding: {
-				top: 5,
-				bottom: 5,
-			},
-			fixedWidth: 0
-		}
-		
-		let menuSettings = {
-			fontFamily: "Courier",
-			fontSize: "20px",
-			color: "#d84c0a",
-			fontStyle: "bold",
-			align: "right",
-			padding: {
-				top: 5,
-				bottom: 5,
-			},
-			fixedWidth: 0
-		}		
-		
-		let mode = "Free-for-all";
-		let playerNum = 1;
 		
 		// show menu text
 		let centerX = game.config.width/2;
-		let centerY = 48;
-		let textSpacer = 29;
+		let centerY = game.config.height/2;
+		let textSpacer = 64;
 		
-		this.add.text(centerX, centerY, "SUPER DINOSAUR REHYDRATOR 3000", menuHeader).setOrigin(0.5);
+		this.add.text(centerX, centerY-textSpacer, "ROCKET PATROL", menuConfig).setOrigin(0.5);
+		this.add.text(centerX, centerY, "Use ← and → to move and [F] to fire", menuConfig).setOrigin(0.5);
+		menuConfig.backgroundColor = "#0F0";
+		menuConfig.color = "#000";
+		this.add.text(centerX, centerY+textSpacer, "Press ← for easy or → for hard", menuConfig).setOrigin(0.5);
 
-		this.add.text(centerX, centerY+textSpacer*2, "P1: Turn with [←] and [→], fire with [↑]", menuBody).setOrigin(0.5);
-		this.add.text(centerX, centerY+textSpacer*3, "P2: Turn with [Q] and [E], fire with [W]", menuBody).setOrigin(0.5);
-		this.add.text(centerX, centerY+textSpacer*4, "P3: Turn with [C] and [B], fire with [V]", menuBody).setOrigin(0.5);
-		this.add.text(centerX, centerY+textSpacer*5, "P4: Turn with [U] and [O], fire with [I]", menuBody).setOrigin(0.5);
-
-		this.add.text(centerX, centerY+textSpacer*7, "Press [1], [2], [3], or [4] to set player number", menuBody).setOrigin(0.5);
-		this.add.text(centerX, centerY+textSpacer*8, "Press [←] for team co-op or [→] for free-for-all", menuBody).setOrigin(0.5);
-
-		this.pNumText = this.add.text(centerX, centerY+textSpacer*10, "Players: "+playerNum, menuSettings).setOrigin(0.5);
-		this.modeText = this.add.text(centerX, centerY+textSpacer*11, "Mode: "+mode, menuSettings).setOrigin(0.5);
-
-		this.add.text(centerX, centerY+textSpacer*13, "Press [ENTER] to start!", menuHeader).setOrigin(0.5);
-
-		// define menu keys
-		key1P = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ONE);
-		key2P = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.TWO);
-		key3P = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.THREE);
-		key4P = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.FOUR);		
-		keyCoop = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-		keyComp = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-		keyStart = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
-		
+		// define keys
+		keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+		keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 	}
 	
 	update() {
-		
-		if (Phaser.Input.Keyboard.JustDown(key1P)) {
-			this.playerNum = 1;
-			this.pNumText.text = "Players: "+this.playerNum;
-		}
-		if (Phaser.Input.Keyboard.JustDown(key2P)) {
-			this.playerNum = 2;
-			this.pNumText.text = "Players: "+this.playerNum;
-		}
-		if (Phaser.Input.Keyboard.JustDown(key3P)) {
-			this.playerNum = 3;
-			this.pNumText.text = "Players: "+this.playerNum;
-		}
-		if (Phaser.Input.Keyboard.JustDown(key4P)) {
-			this.playerNum = 4;
-			this.pNumText.text = "Players: "+this.playerNum;
-		}
-		
-		if (Phaser.Input.Keyboard.JustDown(keyCoop)) {
-			this.mode = "Team co-op";
-			this.modeText.text = "Mode: "+this.mode;
-		}
-		if (Phaser.Input.Keyboard.JustDown(keyComp)) {
-			this.mode = "Free-for-all";
-			this.modeText.text = "Mode: "+this.mode;
-		}
-		
-		if (this.playerNum != 2 && this.playerNum != 4) {
-			this.mode = "Free-for-all";
-			this.modeText.text = "Mode: "+this.mode;
-		}
-		
-		if (Phaser.Input.Keyboard.JustDown(keyStart)) {
-			// start game with chosen settings
+		if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+			// easy mode
 			game.settings = {
-				numPlayers: this.playerNum,
-				gameMode: this.mode,
-				gameTimer: 60000  
+				spaceshipSpeed: 3,
+				gameTimer: 60000    
 			}
-			//this.sound.play("sfx_select");
+			this.sound.play("sfx_select");
 			this.scene.start("playScene");    
 		}
-		
+		if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
+			// hard mode
+			game.settings = {
+				spaceshipSpeed: 4,
+				gameTimer: 45000    
+			}
+			this.sound.play("sfx_select");
+			this.scene.start("playScene");    
+		}
 	}
 
 } 
